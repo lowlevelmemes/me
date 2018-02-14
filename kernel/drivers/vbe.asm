d54b94b670b837cc5676604957c707942b9496af
too-r <william@huddie.net>
2018-02-14 16:33:27 +0000

    More structures, begin writing init routine

--- kernel/drivers/vbe.asm
+++ kernel/drivers/vbe.asm
@@ -64,4 +64,67 @@ vbe_mode_info:
     .framebuffer    dw 0
     .off_mem_off    dw 0
     .off_mem_size   dd 0
-    .reserved1  times 206 db 0
+    .reserved1      times 206 db 0
+
+align 16
+vbe_edid:
+    .padding        times 8 db 0
+    .man_id         dw 0
+    .edid_code      dw 0
+    .serial_no      dd 0
+    .week           db 0
+    .year           db 0
+    .version        db 0
+    .revision       db 0
+    .input          db 0
+    .width_cm       db 0
+    .height_cm      db 0
+    .gamma          db 0
+    .dpms_flags     db 0
+    .chroma         times 10 db 0
+    .timings1       db 0
+    .timings2       db 0
+    .r_timing       db 0
+    .std_timing     times 8 dw 0
+    .timing_desc1   times 18 db 0
+    .timing_desc2   times 18 db 0
+    .timing_desc3   times 18 db 0
+    .timing_desc4   times 18 db 0
+    .reserved       db 0
+    .checksum       db 0
+
+align 32
+; Data structure that stores information about the current state of the screen.
+screen:
+    .width          dd 0
+    .height         dd 0
+    .bpp            dd 0
+    .bytes_per_pix  dd 0
+    .bytes_per_l    dd 0
+    .screen_size    dd 0
+    .screen_size_dqwords dd 0
+    .framebuffer    dd 0
+    .x              dd 0
+    .y              dd 0
+    .x_max          dd 0
+    .y_max          dd 0
+
+; vbe_init - Perform the VBE initialisation.
+vbe_init:
+    push es ; Save the Extra Segment Register - I've read hat some BIOses will destroy ES.
+    mov dword[vbe_info], "VBE2"
+    mov ax, 0x4F00
+    mov di, vbe_info
+    int 0x10
+    pop es
+
+    ; Check if VBE is available
+    cmp ax, 0x4F
+    jne .no_vbe
+
+    cmp dword[vbe_info], "VESA"
+    jne .no_vbe
+.no_vbe:
+    ; TODO: Error print.
+    cli
+    hlt
