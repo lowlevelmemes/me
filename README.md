1456e952b195a8f79c66341c14ca635f333d9fb4
mintsuki <mintsuki@protonmail.com>
2019-09-23 07:35:12 +0200

    lmao lol bro

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # me
 THE meme OS
+
+How to compile:
+Get echfs-utils from [here](https://github.com/qword-os/echfs) and install it
+
+Run `./meke.sh`
+Done.
--- meke.sh
+++ meke.sh
@@ -3,21 +3,20 @@
 set -e
 set -x
 
-gcc -O2 echfs-utils.c -o echfs-utils
 gcc -O2 ./cc/tointel.c -o ./cc/tointel
 
 nasm bootloader/bootloader.asm -f bin -o me.img
 dd bs=32768 count=256 if=/dev/zero >> me.img
 truncate --size=-4096 me.img
-./echfs-utils me.img format 512
+echfs-utils me.img format 512
 
 # compile the C files into assembly first
 ./cc/cc32 kernel/drivers/graphics.c
 
 nasm userland/generic_program.asm -f bin -o generic_program.bin
-./echfs-utils me.img import generic_program.bin generic_program.bin
+echfs-utils me.img import generic_program.bin generic_program.bin
 
 nasm kernel/stub.asm -f bin -o me.bin
-./echfs-utils me.img import me.bin me.bin
+echfs-utils me.img import me.bin me.bin
 
 exit 0
